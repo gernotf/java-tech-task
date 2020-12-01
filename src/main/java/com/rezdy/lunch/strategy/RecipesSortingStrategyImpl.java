ccf1f75f843a4f285390823e124d19ccc803792f
Gernot Fussan <gernot.fussan@gmail.com>
2020-12-01 11:08:39 +1100

    remove unused import

--- src/main/java/com/rezdy/lunch/strategy/RecipesSortingStrategyImpl.java
+++ src/main/java/com/rezdy/lunch/strategy/RecipesSortingStrategyImpl.java
@@ -2,7 +2,6 @@ package com.rezdy.lunch.strategy;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
